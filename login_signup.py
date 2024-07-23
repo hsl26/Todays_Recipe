45a2e25359fccf99060db21431e20d92a3ce94c5
hsl26 <shuala26@gmail.com>
2024-07-23 03:40:38 +0000

    각 페이지 생성 및 수정

---
+++ login_signup.py
@@ -0,0 +1,116 @@
+import streamlit as st
+from streamlit_cookies_controller import CookieController
+
+from recipe import recipe_page
+
+from user_db import *
+
+st.session_state.name = None
+st.session_state.signup_name = None
+# delete_user('1234')
+# db.add_user('1234', '1234', 'example@gmail.com', '1234')
+
+def login_page():
+    with st.form("login_form"):
+        # 페이지 제목
+        st.title("Today's Recipe")
+
+        # 로그인 양식
+        st.header('log-in')
+
+        # 사용자 입력 필드
+        id = st.text_input('id')
+        password = st.text_input('password', type='password')
+
+        # 로그인 버튼
+        if st.form_submit_button('로그인'):
+            # 예시로 간단한 사용자 검증 로직 (실제 프로젝트에서는 데이터베이스 검증 등을 사용)
+            if id_not_exists(id):
+                st.success("회원정보가 없습니다. 회원가입을 진행해주세요")
+            else:
+                # correct_pw = st.session_state.pw_list[st.session_state.id_list.index(id)]
+                if log_in(id, password):
+                    st.session_state.name = get_user_name(id)
+                    st.success(f"환영합니다, {st.session_state.name}님!")
+                    # 로그인 성공 후의 로직을 여기에 추가할 수 있습니다.
+                    # 예를 들어, 사용자의 세션을 관리하는 코드 등.
+                    
+                    st.session_state.page = 'main'
+                    st.session_state.user_id = id
+                    pw, email = get_user_information(st.session_state.user_id)
+                    st.session_state.user_pw = pw
+                    st.session_state.user_email = email
+                    
+                    cookies = CookieController()
+                    cookies.set('logged_in', 'True')
+                    cookies.set('user_id', id)
+                    
+                    # time.sleep(1)
+                    st.rerun()
+                    
+
+                else:
+                    st.error('잘못된 비밀번호입니다.')
+
+        if st.form_submit_button('아직 회원이 아니신가요?'):
+            st.session_state.page = 'signup'
+            st.rerun()
+            
+    # return id
+
+def signup_page():
+    # def append_info(id, pw):
+    #     st.session_state.id_list.append(id)
+    #     st.session_state.pw_list.append(pw)
+
+    if 'id_check' not in st.session_state:
+        st.session_state.id_check = False
+
+    def id_check():
+        st.session_state.id_check = True
+
+    # 페이지 제목
+    st.title('회원가입 화면')
+
+    # 회원가입 양식
+    st.header('회원가입')
+
+    # 사용자 입력 필드
+    id = st.text_input('id')
+
+    if st.button('아이디 중복 확인'):
+        if id not in st.session_state.id_list:
+            id_check()
+            st.success("사용 가능한 아이디 입니다.")
+        else:
+            st.error("사용 불가능한 아이디 입니다. 다시 입력해주세요")
+
+    password = st.text_input('pw', type='password')
+    email = st.text_input('e-mail')
+    fullname = st.text_input('name')
+
+    # 제출 버튼
+    if st.button('회원가입'):
+        if id and password and email and fullname and st.session_state.id_check:
+            # 여기서 실제 회원가입 로직을 추가할 수 있습니다.
+            # 예를 들어, 데이터베이스에 사용자 정보를 저장하는 코드 등.
+            add_user(id, password, email, fullname)
+            # append_info(id, password)
+            st.session_state.page = 'complete'
+            st.session_state.signup_name = fullname
+            st.rerun()
+        else:
+            st.error('모든 필드를 입력과 아이디 중복확인을 마쳐주세요.')
+
+    if st.button('로그인으로 돌아가기'):
+        st.session_state.page = 'login'
+        st.rerun()
+
+def complete_signup_page(id):
+    with st.form("complete_form"):
+        st.subheader(f'🎉{st.session_state.signup_name}님, 회원가입을 환영합니다🎉')
+        st.subheader('서비스를 이용하시려면 로그인을 진행해 주세요.')
+        
+        if st.form_submit_button('로그인 하러 가기'):
+            st.session_state.page = 'login'
+            st.rerun()
---
+++ recipe.py
@@ -0,0 +1,79 @@
+import streamlit as st
+import streamlit.components.v1 as components
+from streamlit_cookies_controller import CookieController
+
+from user_db import get_user_name
+
+import time
+
+import llm_recipe
+
+def navigation_button():
+    cols = st.columns([5, 1]) 
+    with cols[0]:
+        if st.button("홈으로 돌아가기"):
+            st.session_state.page = 'main'
+            st.rerun()
+    with cols[1]:
+        if st.button("마이페이지"):
+            st.session_state.page = 'mypage'
+            st.rerun()
+
+
+def recipe_page(index):
+    cookies = CookieController()
+    st.write(f'안녕하세요 {get_user_name(cookies.get("user_id"))}님')
+
+    if st.button('로그아웃'):
+        
+        cookies.set('logged_in', 'False')
+        cookies.set('user_id', '')
+        st.success('로그아웃 되었습니다.')
+        st.session_state.page = 'login'
+        time.sleep(1)
+        st.rerun()
+
+    # HTML 스타일을 사용한 추가 재료 박스
+    def additional_ingredients(ingred, link, img_url, description):
+        st.markdown(f"""
+                    <div style="padding: 10px; margin-top: 10px; border-bottom: 1px solid #ddd;">
+                        <div style="display: flex; align-items: center;">
+                            <img src="{img_url}" alt="preview" style="width: 100px; height: 100px; object-fit: cover; margin-right: 10px;">
+                            <div>
+                                <ul>
+                                    <p style="font-weight: 700; font-size: 20px;">{ingred}</p>
+                                    <p>{description}</p>
+                                    <p><a href="{link}" target="_blank" style="color: gray; font-style: italic;">{ingred} 구매 링크 바로가기</a></p>
+                             </ul>
+                            </div>
+                        </div>
+                    </div>  
+                    """, unsafe_allow_html=True)
+        
+    
+    # 프롬프트 엔지니어링으로 재료 리스트만 따로 빼고 전처리.
+    need_ingredient = set(['계란', '소금', '밥', '대파', '당근', '올리브 오일'])
+    # 추후 DB에서 받아오기
+    have_ingredient = set(['계란', '소금', '올리브 오일', '밥', '양배추', '딸기']) 
+    add_ingredient = need_ingredient - have_ingredient
+
+    # st.title('메뉴 이름')
+    
+    navigation_button()
+
+    # st.header('재료')
+    st.header('레시피')
+    
+    food_name = st.session_state.get("food_name", "정보가 없습니다.")
+    
+    # llm에 해당 음식 이름 전달 및 결과 반환
+    recipe_info = llm_recipe.GetInformation(food_name)
+    st.markdown(f"{recipe_info}")
+    
+    st.header('추가구매 추천 재료')
+    if add_ingredient:
+        for ingred in list(add_ingredient):
+            purchase_link = f"https://www.kurly.com/search?sword={ingred}"
+            img_url = "https://res.kurly.com/images/marketkurly/logo/logo_sns_marketkurly.jpg"  # 예시 이미지 URL을 적절히 변경
+            description = "마켓 컬리에서 찾은 관련 상품입니다."
+            additional_ingredients(ingred, purchase_link, img_url, description)
