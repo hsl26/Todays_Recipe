329b3d820322002a5e58c0bbe5eaa95da5829f73
ahyng <ahyng1@gmail.com>
2024-07-23 11:12:39 +0900

    로그인/회원가입 구현

---
+++ main.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from streamlit_cookies_controller import CookieController
+from login_signup import login_page
+from login_signup import signup_page
+from login_signup import complete_signup_page
+from recipe import recipe_page
+from user_db import get_user_name
+# ex_id_list = ["ksj020919", "abcd1234", "efgh5678", "1234"]
+# ex_pw_list = ["kys101014!", "ABCD1234!", "EFGH5678!", "1234"]
+logged_in=st.session_state.logged_in = None
+st.session_state.user_name = None
+cookies = CookieController()
+
+
+if cookies.get("logged_in") == 'True':
+    st.session_state.page = 'recipe'
+elif 'page' not in st.session_state:
+    st.session_state.page = 'login'
+
+# if 'id_list' not in st.session_state:
+#     st.session_state.id_list = ex_id_list
+# if 'pw_list' not in st.session_state:    
+#     st.session_state.pw_list = ex_pw_list
+if st.session_state.page == 'login':
+    st.session_state.user_name = None
+# 현재 페이지 상태에 따라 화면 표시
+if st.session_state.page == 'login':
+    login_page()
+elif st.session_state.page == 'signup':
+    signup_page()
+elif st.session_state.page == 'recipe':
+    recipe_page()
+elif st.session_state.page == 'complete':
+    complete_signup_page()
---
+++ recipe.py
@@ -0,0 +1,51 @@
+import streamlit as st
+from streamlit_cookies_controller import CookieController
+import streamlit.components.v1 as components
+import time
+
+def recipe_page():
+    cookies = CookieController()
+    st.write(f'안녕하세요 {cookies.get("user_name")}님')
+
+    if st.button('로그아웃'):
+        
+        cookies.set('logged_in', 'False')
+        cookies.set('user_name', '')
+        st.success('로그아웃 되었습니다.')
+        st.session_state.page = 'login'
+        time.sleep(1)
+        st.rerun()
+
+    # HTML 스타일을 사용한 추가 재료 박스
+    def additional_ingredients(ingred, link, img_url, description):
+        st.markdown(f"""
+                    <div style="padding: 10px; margin-top: 10px; border-bottom: 1px solid #ddd;">
+                        <div style="display: flex; align-items: center;">
+                            <img src="{img_url}" alt="preview" style="width: 100px; height: 100px; object-fit: cover; margin-right: 10px;">
+                            <div>
+                                <ul>
+                                    <p style="font-weight: 700; font-size: 20px;">{ingred}</p>
+                                    <p>{description}</p>
+                                    <p><a href="{link}" target="_blank" style="color: white; font-style: italic;">{ingred} 구매 링크 바로가기</a></p>
+                             </ul>
+                            </div>
+                        </div>
+                    </div>  
+                    """, unsafe_allow_html=True)
+        
+        
+    need_ingredient = set(['계란', '소금', '밥', '대파', '당근', '올리브 오일'])
+    have_ingredient = set(['계란', '소금', '올리브 오일', '밥', '양배추', '딸기'])
+    add_ingredient = need_ingredient - have_ingredient
+
+    st.title('메뉴 이름')
+
+    st.header('재료')
+    st.header('레시피')
+    st.header('추가구매 추천 재료')
+    if add_ingredient:
+        for ingred in list(add_ingredient):
+            purchase_link = f"https://www.kurly.com/search?sword={ingred}"
+            img_url = "https://res.kurly.com/images/marketkurly/logo/logo_sns_marketkurly.jpg"  # 예시 이미지 URL을 적절히 변경
+            description = "마켓 컬리에서 찾은 관련 상품입니다."
+            additional_ingredients(ingred, purchase_link, img_url, description)
